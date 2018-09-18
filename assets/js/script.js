b5eea3b30e186bb8b86dcfc6323d7beba425fb20
Guilherme Ortiz <gui.ortiz@live.com>
2018-09-18 10:47:23 -0300

    desenvolvimento da home e parte da quem somos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,36 @@
+var qntItens = 0;
+
 $(document).ready(function(){
 
-    $('.btn-link').on('click', function(){
-        alert('to vivo !');
-    });
-    
-    // $('.navbar-toggler').on('click', function(){
-    //     $('.collapse.navbar-collapse').toggleClass('show');
-    //     $('.collapse.navbar-collapse').removeClass('collapsin');
-    //     $('.navbar-nav').toggleClass('active');
-    // });
-
-    $("#slider").on('slide.bs.carousel', function(evt) {
-        
-        var step = $(evt.relatedTarget).index();
-
-        $('#slider_captions .carousel-caption:not(#caption-'+step+')').fadeOut('fast', function() {
-                $('#caption-'+step).fadeIn();
+    $("#carouselExampleControls").on('slide.bs.carousel', function () {
+        $( ".carousel-indicators li" ).each(function(element) {
+            if($(this).hasClass('active')) {
+                //console.log('O li que tem active é: ' +element);
+                var slide = $(this).data('slide-to');
+                //console.log('O slide é: ' + (slide));
+                mudaSlide((slide));
+            }
         });
-
     });
+    $('.carousel-caption .carousel-item').each(function(i){
+        qntItens = i;
+        console.log('contei, vou derterminar o ultimo' + qntItens);
+    });
+
 });
+
+
+
+function mudaSlide(slide){
+    slide = parseInt(slide+1);
+    //console.log('vou clocar no li posicao: ' + slide);
+    if (slide < (qntItens+1) ) {
+        $('.carousel-inner .carousel-item[data-slider='+slide+']').removeClass('active');
+        $('.carousel-inner .carousel-item[data-slider='+(slide+1)+']').addClass('active');
+    }
+    else {
+        $('.carousel-inner .carousel-item[data-slider='+slide+']').removeClass('active');
+        $('.carousel-inner .carousel-item[data-slider='+1+']').addClass('active');
+    }
+    
+}
